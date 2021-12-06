521ceace30862a9fa29ccb07a6ebe93a92decc58
Daniel Hill <daniel.s.hill01@gmail.com>
2021-12-06 15:08:31 -0700

    added create slime form

--- src/pages/SlimesCreate.js
+++ src/pages/SlimesCreate.js
@@ -1,9 +1,14 @@
 import { useState } from "react";
-import { withRouter } from 'react-router-dom'
+import { withRouter } from "react-router-dom";
 
 function SlimesCreate(props) {
   const [newForm, setNewForm] = useState({
     name: "",
+    dietType: "",
+    favFood: "",
+    favToy: "",
+    slimeType: "",
+    color: "",
     image: "",
   });
 
@@ -19,9 +24,14 @@ function SlimesCreate(props) {
     props.createSlimes(newForm);
     setNewForm({
       name: "",
+      dietType: "",
+      favFood: "",
+      favToy: "",
+      slimeType: "",
+      color: "",
       image: "",
     });
-    props.history.push('/slimes');
+    props.history.push("/slimes");
   };
 
   return (
@@ -31,14 +41,49 @@ function SlimesCreate(props) {
           type="text"
           value={newForm.name}
           name="name"
-          placeholder="name"
+          placeholder="Name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={newForm.dietType}
+          name="dietType"
+          placeholder="Type of Diet"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={newForm.favFood}
+          name="favFood"
+          placeholder="Favorite Food"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={newForm.favToy}
+          name="favToy"
+          placeholder="Favorite Toy"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={newForm.slimeType}
+          name="slimeType"
+          placeholder="Type of Slime"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={newForm.color}
+          name="color"
+          placeholder="Color"
           onChange={handleChange}
         />
         <input
           type="text"
           value={newForm.image}
           name="image"
-          placeholder="image URL"
+          placeholder="Image URL"
           onChange={handleChange}
         />
         <input type="submit" value="Create Slime" />
